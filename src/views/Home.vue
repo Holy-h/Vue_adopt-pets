5702dedbfe97c21bd50890970035fd301d57830f
bonggt <kepy1106@gmail.com>
2020-03-13 22:03:56 +0900

    Form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Adopt a new best friend.</h1>
+    <button class="btn btn-primary" @click="togglePetForm">새 친구 추가하기</button>
+    <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
+      <b-form-group id="input-group-2" label="펫의 이름:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="formData.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="종:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="formData.species"
+          :options="['cats', 'dogs']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="나이:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="formData.age"
+            required
+            placeholder="Enter name"
+          ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      showPetForm: false,
+      formData: {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
+  },
+  methods: {
+    ...mapActions([
+      'addPet'
+    ]),
+    togglePetForm () {
+      this.showPetForm = !this.showPetForm
+    },
+    handleSubmit () {
+      const { species, age, name } = this.formData
+      const payload = {
+        species,
+        pet: {
+          name,
+          age
+        }
+      }
+      this.addPet(payload)
+    }
   }
 }
 </script>
---
+++ src/views/Pet.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Pet Detail View</h1>
+        <h3>{{ animal.name }} ({{ $route.params.species }})</h3>
+        <p>나이: {{ animal.age }}살</p>
+        <p>종: {{ animal.breed }}</p>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  methods: {
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+</script>
